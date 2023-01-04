8cd9fc31a0bcf9b85e1320f13a091bbcbe7ec2b4
eyupGuc <meydanguresi@gmail.com>
2023-01-04 07:06:12 +0300

    router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,51 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem quibusdam
+      doloremque dolorum cumque iusto repudiandae quisquam modi adipisci odio
+      sapiente, autem delectus? Fugit amet voluptatibus minus libero! Assumenda
+      dolore vitae, maiores obcaecati aliquid repudiandae, nihil ducimus unde
+      dicta velit voluptatum distinctio, voluptates accusamus consequatur illo
+      provident vel numquam sunt ipsa aperiam autem. Dicta doloribus ea
+      molestias consequatur esse iure maiores inventore incidunt. Dolores unde
+      quis cumque, facilis pariatur dolorum corporis totam a, iusto, aliquam
+      nemo consequatur aliquid et. Facere consectetur blanditiis inventore illo
+      recusandae ea. Deserunt veniam accusantium eveniet officia sed, a
+      molestias accusamus quis incidunt vel dolore culpa ratione unde inventore
+      alias iste hic possimus voluptatem id quibusdam dolor nulla aspernatur
+      repellat eius. Perferendis quas cupiditate at eligendi, excepturi magnam
+      voluptatibus vero repudiandae quia possimus, odit ipsum distinctio odio ut
+      quidem, molestias dolorum. Culpa officiis sint veniam quo, perspiciatis
+      asperiores quisquam aliquid commodi praesentium cupiditate minima
+      laboriosam at quod voluptatem vero maiores corrupti corporis ducimus
+      itaque, inventore minus a, illo nulla. Earum maiores, totam reprehenderit,
+      provident deleniti quod in delectus deserunt consectetur ab nisi dolore
+      cupiditate similique quasi! Repellat ullam facilis quisquam molestias
+      commodi, suscipit eligendi culpa officia odio. Dolorem labore nisi nulla
+      dolore ad esse vero rem culpa iure dicta ratione, omnis tenetur iusto
+      porro alias unde cum quas animi, laboriosam sit enim! Beatae fugiat sint
+      iusto animi quae facere, libero exercitationem enim veniam, modi
+      accusantium repellat quisquam, ipsam non aliquid est dicta! Facilis
+      consequatur repudiandae et repellendus quo qui recusandae optio ipsum ea
+      earum culpa, quia assumenda accusamus, amet, ratione totam blanditiis.
+      Minima amet vitae ab neque ut soluta velit beatae repellat veritatis!
+      Quam, veritatis similique a quos sint modi esse consequatur aperiam minus
+      nam commodi voluptas iure delectus ut voluptate. Libero temporibus,
+      tenetur fugit labore incidunt id odio cum tempore laudantium repellendus,
+      aliquam consequatur similique blanditiis? Numquam similique quae ea
+      suscipit dolorem! Qui at porro vitae aliquam odio adipisci consequuntur,
+      iure fugiat. Iste quos a dolorem beatae recusandae dicta velit ab
+      voluptatibus cupiditate odit, atque delectus dolore sed possimus maiores
+      nesciunt fuga consequatur adipisci minima, veniam rerum nam. Dolor hic
+      beatae dolorum fugiat commodi dolores, at cumque error ratione maxime,
+      sint optio eaque, veniam laudantium nulla impedit sed recusandae ad aut.
+      Voluptatibus, dolore corrupti. Tempora quidem perferendis perspiciatis
+      deserunt! Dolorem ad quaerat possimus nostrum, asperiores optio,
+      perspiciatis totam in sunt, illum eum eius? Reprehenderit quas, nulla
+      earum, nostrum, suscipit repellendus doloremque distinctio nihil obcaecati
+      iste deserunt.
+    </div>
+  );
 };
 
 export default Home;
