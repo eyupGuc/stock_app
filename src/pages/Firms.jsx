e86e49cdf24f9b2dd37346ebe455e4e5955908d3
eyupGuc <meydanguresi@gmail.com>
2023-01-06 08:26:33 +0300

    headers

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://14223.fullstack.clarusway.com/";
+
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
   return <div>Firms</div>;
 };
 
