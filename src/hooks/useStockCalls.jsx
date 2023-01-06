a95f96280fef7c18a64813c66c29df2b393ce019
eyupGuc <meydanguresi@gmail.com>
2023-01-06 15:04:33 +0300

    card

--- src/hooks/useStockCalls.jsx
+++ src/hooks/useStockCalls.jsx
@@ -1,11 +1,12 @@
-import { axiosWithToken } from "../service/axiosInstance";
+
 import { useDispatch } from "react-redux";
 import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useAxios from "./useAxios";
 
 
 const useStockCalls = () => {
   const dispatch = useDispatch();
- 
+ const {axiosWithToken}=useAxios()
 
   const getStockData = async (url) => {
     dispatch(fetchStart());
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,15 +1,35 @@
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
 import useStockCalls from "../hooks/useStockCalls";
 
 const Firms = () => {
-  const { getFirms,getSales } = useStockCalls();
+  const { getFirms } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
+  // console.log(firms)
 
   useEffect(() => {
     getFirms();
-    getSales();
   }, []);
 
-  return <div>Firms</div>;
+  return (
+    <Box>
+      <Typography variant="h4" color="error" mb={4}>
+        Firms
+      </Typography>{" "}
+      <Button variant="contained">New Firm</Button>
+      {firms?.length > 0 && (
+        <Grid container justifyContent="center" gap={4}>
+          {firms?.map((item) => (
+            <Grid item key={item.id}>
+              <FirmCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default Firms;
