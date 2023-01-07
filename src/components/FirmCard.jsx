a14ebd6cf3b1981b44bf73e903086b3df41dd8b7
eyupGuc <meydanguresi@gmail.com>
2023-01-07 18:55:40 +0300

    move

--- src/components/FirmCard.jsx
+++ src/components/FirmCard.jsx
@@ -9,25 +9,36 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { btnHoverStyle } from "../styles/globalStyle";
 import useStockCalls from "../hooks/useStockCalls";
+import { CardHeader } from "@mui/material";
 
 export default function FirmCard({ item }) {
   const { deleteFirm } = useStockCalls();
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        p: 2,
+        maxWidth: "300px",
+        maxHeight: "600px",
+        minHeight: "400px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <CardHeader title={item?.name} subheader={item?.address} />
       <CardMedia
         component="img"
         alt="green iguana"
-        height="140"
+        height="325"
+        width="250"
         image={item?.image}
+        sx={{ p: 1, objectFit: "contain" }}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography variant="body2" color="text.secondary">
           {item?.phone}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {item?.address}
-        </Typography>
+       
       </CardContent>
       <CardActions sx={{ display: "flex", justifyContent: "center" }}>
         <EditIcon sx={btnHoverStyle} />
